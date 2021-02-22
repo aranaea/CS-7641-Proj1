ae94c164c12942d76200b2014307bf6f3205c4d5
Peter Cornelius <peter@grabango.com>
2021-02-21 16:37:44 -0800

    Complete?

--- Project1/src/omscs_7641_p1/SVM.py
+++ Project1/src/omscs_7641_p1/SVM.py
@@ -3,8 +3,56 @@ from time import time
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 from omscs_7641_p1.datasets import all_datasets
 from omscs_7641_p1.plots import draw_learning_curve
+
+
+def kernel_plots(train_X, train_Y, test_X, test_Y, dataset_name):
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+    scores = []
+
+    clf = SVC()
+    for kernel in kernels:
+        start = time()
+        print(kernel)
+        clf.kernel = kernel
+        clf.fit(train_X, train_Y)
+        score = clf.score(test_X, test_Y)
+        scores.append(score)
+        print(f"  {time() - start}::{score}")
+
+    ax = plt.subplot()
+    ax.grid(zorder=0)
+    ax.set_title(f"SVM Kernel {dataset_name}")
+    ax.bar(kernels, scores, zorder=3)
+    ax.set_ylabel("Accuracy")
+    plt.show()
+
+
+def degree_plots(train_X, train_Y, test_X, test_Y, dataset_name):
+    scores = []
+    degrees = list(set(np.linspace(3, int(len(train_X.columns)), 5, dtype=np.int16)))
+
+    print(f"{dataset_name}.{len(degrees)}")
+    clf = SVC()
+    for degree in degrees:
+        start = time()
+        print(f"  {degree}")
+        clf.degree = degree
+        clf.fit(train_X, train_Y)
+        score = clf.score(test_X, test_Y)
+        scores.append(score)
+        print(f"  {time() - start}::{score}")
+
+    ax = plt.subplot()
+    ax.grid(zorder=0)
+    ax.set_title(f"SVM Poly Dim {dataset_name}")
+    ax.plot(degrees, scores, "o-", zorder=3)
+    ax.set_ylabel("Accuracy")
+    ax.set_xlabel("Poly Degrees")
+    plt.legend(loc='best')
+    plt.show()
 
 
 def kernel_search(train_X, train_Y, test_X, test_Y, dataset_name):
@@ -33,11 +81,17 @@ def degree_search(train_X, train_Y, test_X, test_Y, dataset_name):
 
 def main():
     for dataset_name, train_X, test_X, train_Y, test_Y in all_datasets():
-        start = time()
-        # params = kernel_search(train_X, train_Y, test_X, test_Y, dataset_name)
-        param_update = degree_search(train_X, train_Y, test_X, test_Y, dataset_name)
-
-
+        params = kernel_search(train_X, train_Y, test_X, test_Y, dataset_name)
+        print(params)
+        params = degree_search(train_X, train_Y, test_X, test_Y, dataset_name)
+        print(params)
+        kernel_plots(train_X, train_Y, test_X, test_Y, dataset_name)
+        degree_plots(train_X, train_Y, test_X, test_Y, dataset_name)
+        clf = SVC()
+        if dataset_name == "Diamonds":
+            clf.kernel = 'linear'
+        clf.fit(train_X, train_Y)
+        draw_learning_curve(clf, test_X, test_Y, dataset_name)
 
 
 if __name__ == '__main__':
